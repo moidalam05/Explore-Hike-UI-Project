ed46f7f36e24d33a67ced51805d7f5809fd7f840
moidalam05 <moidalam005@hotmail.com>
2024-06-27 13:40:06 +0530

    This is the Final project ui desined

---
+++ index.js
@@ -0,0 +1,19 @@
+// Navbar toggerler
+const navDialog = document.getElementById("nav-dialog");
+
+function handleMenu() {
+	navDialog.classList.toggle("hidden");
+}
+
+// // Scroll to horizontal cards
+// const scrollContainer = document.getElementById("scrollContainer");
+// const nextButton = document.getElementById("next");
+// const prevButton = document.getElementById("prev");
+
+// nextButton.addEventListener("click", () => {
+// 	scrollContainer.scrollBy({ left: 200, behavior: "smooth" });
+// });
+
+// prevButton.addEventListener("click", () => {
+// 	scrollContainer.scrollBy({ left: -200, behavior: "smooth" });
+// });
