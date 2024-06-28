a88c098722460351abf05cd4f1e191f4075e928d
moidalam05 <moidalam005@hotmail.com>
2024-06-28 13:21:06 +0530

    Refactor CSS styles for better readability and maintainability

--- index.js
+++ index.js
@@ -5,15 +5,17 @@ function handleMenu() {
 	navDialog.classList.toggle("hidden");
 }
 
-// // Scroll to horizontal cards
-// const scrollContainer = document.getElementById("scrollContainer");
-// const nextButton = document.getElementById("next");
-// const prevButton = document.getElementById("prev");
+// Scroll to horizontal cards
+const scrollContainer = document.getElementById("scrollContainer");
+const nextButton = document.getElementById("next");
+const prevButton = document.getElementById("prev");
+console.log(nextButton, prevButton);
 
-// nextButton.addEventListener("click", () => {
-// 	scrollContainer.scrollBy({ left: 200, behavior: "smooth" });
-// });
+nextButton.addEventListener("click", () => {
+	console.log("clicket");
+	scrollContainer.scrollBy({ left: 200, behavior: "smooth" });
+});
 
-// prevButton.addEventListener("click", () => {
-// 	scrollContainer.scrollBy({ left: -200, behavior: "smooth" });
-// });
+prevButton.addEventListener("click", () => {
+	scrollContainer.scrollBy({ left: -200, behavior: "smooth" });
+});
